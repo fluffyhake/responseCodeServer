937270741ec5b8595d947b2e3d2ad4f663eaf0a8
fluffyhake <56997877+fluffyhake@users.noreply.github.com>
2024-08-03 23:36:45 +0200

    works with feroxbuster

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # responseCodeServer
 A node server that can respond with different error codes based on user parameters.
+
+
+Generate a cert and put it in the certs folder:
+
+certs/
+    |  cert.pem
+    |  key.pem
---
+++ src/app.ts
@@ -0,0 +1,56 @@
+import express, { Express, Request, Response } from "express";
+import dotenv from "dotenv";
+import { v4 as uuid } from 'uuid';
+import https from 'https';
+import fs from 'fs';
+import path from 'path';
+
+const app: Express = express();
+const port = 3000;
+let counter: number = 0
+
+
+app.get("*", (req: Request, res: Response) => {
+    const response: String = uuid();
+
+    // if(req.url.slice(-1) === "/"){
+    //     res.status(403).send()
+    // }
+
+
+    const min = 1;
+    const max = 6000;
+    const random = Math.floor(Math.random() * (max - min + 1)) + min;
+    // Reset counter after reaching 10
+
+    if (random === 1 || random === 2) {
+        res.status(200).send("Express + TypeScript Server " + response);
+    } else if (random === 3) {
+        // res.status(301).redirect("http://172.28.48.221:3000/dsaadl" + random);
+        res.status(403).send("Express + TypeScript Server " + response);
+    } else if (random === 4 && req.url.slice(-1) !== "/"){
+        res.status(301)
+        res.setHeader("Location", req.url + "/")
+        res.send()
+
+    }
+    else{
+        res.status(404).send()
+    }
+});
+
+
+// app.listen(port, () => {
+//     console.log(`[server]: Server is running at http://localhost:${port}`);
+// });
+
+
+const httpsServer = https.createServer({
+    cert: fs.readFileSync(path.resolve(__dirname, '../certs/cert.pem')),
+    key: fs.readFileSync(path.resolve(__dirname, '../certs/key.pem'))
+  }, app);
+  
+  httpsServer
+   .listen(3000, () => {
+      console.log('Server listening on port 3000. URL: https://localhost:3000');
+    });
